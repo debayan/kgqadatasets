b84624e8c48daed9cae78946f889fe5e0a4887b8
Debayan Banerjee <debayanin@gmail.com>
2021-03-01 22:47:34 +0100

    Added sparql validity checks

--- lcquad1/stats.py
+++ lcquad1/stats.py
@@ -1,9 +1,14 @@
-import sys,os,json
+import sys,os,json,rdflib
+
+g = rdflib.Graph()
+#g.load('https://www.wikidata.org/wiki/Special:EntityData/Q42.rdf')
 
 d = json.loads(open(sys.argv[1]).read())
 
 em = 0
 nem = 0
+valid = 0
+invalid = 0
 for idx,item in enumerate(d):
     print(item['uid'])
     print(item['question'])
@@ -26,5 +31,14 @@ for idx,item in enumerate(d):
             print(target)
             print(answer)
             nem+=1
+    try:
+        answer = answer.replace('entpos@@1','http://www.wikidata.org/entity/Q42').replace('entpos@@2','http://www.wikidata.org/entity/Q42').replace('predpos@@1','http://www.wikidata.org/entity/P1477').replace('predpos@@2','http://www.wikidata.org/entity/P1477')
+        qres = g.query(answer)
+        valid += 1
+        print("valid sparql")
+    except Exception as err:
+        print("invalid sparql ",err)
+        invalid += 1
     print('................')
-print("exactmatch: ",em, "  notmatch: ",nem,idx)
+print("exactmatch: ",em, "  notmatch: ",nem," total: ",idx)
+print("validsparql: ",valid," nonvalidsparql: ",invalid)
