652998d5434a953a7f202295113daf36b6213f03
Debayan Banerjee <debayanin@gmail.com>
2021-03-02 18:10:15 +0100

    fixed variable name ordering

--- lcquad1/reducevars.py
+++ lcquad1/reducevars.py
@@ -10,9 +10,11 @@ for item in d:
     sparql = item['sparql_query']
     sparql_split = sparql.replace('(',' ( ').replace(')',' ) ').replace('{',' { ').replace('}',' } ').split()
     print(sparql)
-    variables = set([x for x in sparql_split if x[0] == '?'])
-    print(variables)
-    for idx,var in enumerate(variables):
+    #variables = set([x for x in sparql_split if x[0] == '?'])
+    oldvars = [x for x in sparql_split if x[0] == '?']
+    oldvarset = list(dict.fromkeys(oldvars).keys()) 
+    print(oldvarset)
+    for idx,var in enumerate(oldvarset):
         sparql = sparql.replace(var,newvars[idx])
     print(sparql)
     newitem['sparql_query_reduced_vars'] = sparql
