a5140cc75cc5c761d85297770f31cac61267dd53
Debayan Banerjee <debayanin@gmail.com>
2021-03-13 17:13:16 +0100

    Add F1

--- lcquad1/kbstats.py
+++ lcquad1/kbstats.py
@@ -1,9 +1,51 @@
 import sys,os,json,rdflib,re,copy,requests
 
 
+
+def calcf1(target,answer):
+    if target == answer:
+        return 1.0
+    try:
+        tb = target['results']['bindings']
+        rb = answer['results']['bindings']
+        tp = 0
+        fp = 0
+        fn = 0
+        for r in rb:
+            if r in tb:
+                tp += 1
+            else:
+                fp += 1
+        for t in tb:
+            if t not in rb:
+                fn += 1
+        precision = tp/float(tp+fp+0.001)
+        recall = tp/float(tp+fn+0.001)
+        f1 = 2*(precision*recall)/(precision+recall+0.001)
+        print("f1: ",f1)
+        return f1
+    except Exception as err:
+        print(err)
+    try:
+        if target['boolean'] == answer['boolean']:
+            print("boolean true/false match")
+            f1 = 1.0
+            print("f1: ",f1)
+        if target['boolean'] != answer['boolean']:
+            print("boolean true/false mismatch")
+            f1 = 0.0
+            print("f1: ",f1)
+            return f1
+    except Exception as err:
+        f1 = 0.0
+        print("f1: ",f1)
+        return f1 
+        
+             
 def hitkg(query):
     try:
         url = 'http://localhost:8892/sparql/'
+        print(query)
         r = requests.get(url, params={'format': 'json', 'query': query})
         json_format = r.json()
         #print(entid,json_format)
@@ -15,7 +57,7 @@ def hitkg(query):
 
 goldd = {}
 goldq = {}
-d = json.loads(open(sys.argv[1]).read()) #lcq1gold
+d = json.loads(open(sys.argv[1]).read()) # test-data.json  (lcq1 test file)
 
 for item in d:
     result = hitkg(item['sparql_query'])
@@ -30,6 +72,7 @@ em = 0
 nem = 0
 qem = 0
 qnem = 0
+totf1 = 0.0
 for idx,item in enumerate(d):
     print(item)
     print(str(item['uid']))
@@ -62,6 +105,9 @@ for idx,item in enumerate(d):
         if rel:
             answer = answer.replace('predpos@@'+str(idx1+1),rel)
     resultanswer = hitkg(answer)
+    f1  = calcf1(resulttarget,resultanswer)
+    totf1 += f1
+    avgf1 = totf1/float(idx+1)
     if resulttarget == resultanswer:
         print("match")
         em += 1
@@ -76,3 +122,4 @@ for idx,item in enumerate(d):
     print('................')
     print("exactmatch: ",em, "  notmatch: ",nem," total: ",idx)
     print("querymatch: ",qem," querynotmatch: ",qnem)
+    print("avg f1: ",avgf1)
